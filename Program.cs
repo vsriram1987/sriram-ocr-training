abe6996dc2a1d40dca8284ae26351b906ecb8a78
Sriram <v.sriram.1987@gmail.com>
2020-06-21 10:30:01 +0530

    New OCR Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sriram_ocr_training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
